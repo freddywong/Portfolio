9da5a0759fe72d39b99cb76ff6b416d158b20da1
Frederick Wong <frederick.kf.wong@gmail.com>
2015-01-23 14:34:51 +1100

    Completed home and project page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'home' => 'pages#home'
   get 'about' => 'pages#about'
   get 'projects' => 'pages#projects'
+  get 'contact' => 'pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
