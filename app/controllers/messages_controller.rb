ce05a4607e6198f86abad715b8db3a57a3edf44d
Frederick Wong <frederick.kf.wong@gmail.com>
2015-01-25 23:37:57 +1100

    Still fixing heroku sneding email error

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     
     if @message.valid?
-      MessageMailer.new_message(@message).deliver_now
+      MessageMailer.new_message(@message).deliver
       redirect_to contact_path, notice: "Your messages has been sent."
     else
       flash[:alert] = "An error occurred while delivering this message."
